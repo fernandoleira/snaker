85189a54d15479d22502674fcb2e3d9ce808c0a6
Fernando Leira <LeiraFernandoCortel@gmail.com>
2023-10-29 22:51:34 -0700

    Add background sprites and create tilemap in World scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,6 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	print(input_vector)
 	
 	velocity = input_vector.normalized() * MAX_SPEED
 	position += velocity * delta
